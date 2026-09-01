196e5b71c90b3ee69e579c4753a0269da90b3457
VuKV03 <khuatvanvu03@gmail.com>
2026-09-01 22:36:25 +0700

    Infrastructure

---
+++ api-net10/Todo.Infrastructure/ConfigureServices.cs
@@ -0,0 +1,49 @@
+﻿using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+using SharedKernel.Application.Behaviours;
+using SharedKernel.Infrastructure;
+using Todo.Application.Commands;
+using Todo.Application.Interfaces;
+using Todo.Infrastructure.Data;
+
+namespace Todo.Infrastructure;
+
+public static class ConfigureServices
+{
+    public static IServiceCollection AddTodoInfrastructureServices(
+        this IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection")
+            ?? throw new InvalidOperationException("Missing connection string 'DefaultConnection'.");
+
+        // Đăng ký AuditableEntitySaveChangesInterceptor (+ hasher/jwt service dùng chung)
+        services.AddSharedKernelInfrastructureServices();
+
+        services.AddDbContext<TodoDbContext>(options =>
+        {
+            options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString), mySqlOptions =>
+            {
+                mySqlOptions.SchemaBehavior(MySqlSchemaBehavior.Ignore);
+            });
+        });
+
+        services.AddScoped<ITodoDbContext>(provider => provider.GetRequiredService<TodoDbContext>());
+
+        // Quét assembly Todo.Application: MediatR handler + validator + AutoMapper profile
+        var appAssembly = typeof(CreateTodoCommand).Assembly;
+
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(appAssembly);
+            cfg.AddOpenBehavior(typeof(ValidationBehaviour<,>));
+        });
+
+        services.AddValidatorsFromAssembly(appAssembly);
+        services.AddAutoMapper(cfg => { }, appAssembly);
+
+        return services;
+    }
+}
---
+++ api-net10/Todo.Infrastructure/Data/TodoDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SharedKernel.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Todo.Application.Interfaces;
+using Todo.Domain.Entities;
+
+namespace Todo.Infrastructure.Data
+{
+    public class TodoDbContext : BaseDbContext<TodoDbContext>, ITodoDbContext
+    {
+        public TodoDbContext(DbContextOptions<TodoDbContext> options, 
+            AuditableEntitySaveChangesInterceptor auditableInterceptor)
+            : base(options, auditableInterceptor)
+        { 
+        }
+
+        public DbSet<cong_viec> cong_viec { get; set; } = null;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(TodoDbContext).Assembly);
+        }
+    }
+}
