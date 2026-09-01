b716ffe7ca83e1b3e89879a01389f6d06b47ece8
VuKV03 <khuatvanvu03@gmail.com>
2026-09-01 19:57:33 +0700

    domain

---
+++ api-net10/Todo.Domain/Entities/cong_viec.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using SharedKernel.Domain.Entities;
+using Todo.Domain.Enums;
+
+namespace Todo.Domain.Entities;
+
+[Table("cong_viec")]
+public class cong_viec : BaseAuditableEntity
+{
+    [Required]
+    public Guid nguoi_dung_id { get; set; }
+
+    [Required]
+    [StringLength(500)]
+    public string tieu_de { get; set; } = string.Empty;
+
+    public TrangThaiCongViec trang_thai { get; set; } = TrangThaiCongViec.ChuaLam;
+
+    public DateTime? ngay_hoan_thanh { get; set; }
+}
