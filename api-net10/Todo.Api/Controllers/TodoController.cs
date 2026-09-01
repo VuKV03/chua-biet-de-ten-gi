93ed50c4a2b989771a1b7e987e5b1de70e55aa4d
VuKV03 <khuatvanvu03@gmail.com>
2026-09-01 23:19:53 +0700

    controller

---
+++ api-net10/Todo.Api/Controllers/TodoController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SharedKernel.Api.Base;
+using SharedKernel.Application.Exceptions;
+using Todo.Application.Commands;
+using Todo.Application.DTO;
+using Todo.Application.Queries;
+
+namespace Todo.Api.Controllers;
+
+[Route("api/todo")]
+[ApiController]
+[Authorize]                       // áp cho mọi action, khỏi lặp từng cái
+public class TodoController : BaseApiController
+{
+    [HttpPost]
+    public async Task<ActionResult<TodoDto>> Create([FromBody] CreateTodoCommand request)
+    {
+        try
+        {
+            return Ok(await Mediator.Send(request));
+        }
+        catch (Exception ex)
+        {
+            var err = ErrorCtr.ExtractErrorInfo(ex); // ← "dịch" exception thành status code + message
+            return StatusCode((int)err.errorCode, err.errors); // ← trả về đúng status code đó
+        }
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<TodoDto>> GetById(Guid id)
+    {
+        try
+        {
+            return Ok(await Mediator.Send(new GetTodoByIdQuery { id = id }));
+        }
+        catch (Exception ex)
+        {
+            var err = ErrorCtr.ExtractErrorInfo(ex);
+            return StatusCode((int)err.errorCode, err.errors);
+        }
+    }
+}
--- api-net10/Todo.Api/Program.cs
+++ api-net10/Todo.Api/Program.cs
@@ -1,41 +1,38 @@
+using SharedKernel.Api;
+using SharedKernel.Api.Extensions;
+using Todo.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// Tắt log cảnh báo thiếu license key của MediatR/AutoMapper (bản Community, không chặn chạy)
+builder.Logging.AddFilter("LuckyPennySoftware", LogLevel.None);
+
+builder.Services.AddSharedKernelApiServices();                          // IIdentityService, HttpContextAccessor
+builder.Services.AddTodoInfrastructureServices(builder.Configuration);  // DbContext, MediatR, Validator, Mapper
+
+builder.Services.AddControllers();
+builder.Services.AddSharedKernelSwagger("Todo API (.NET 10)");
+builder.Services.AddSharedKernelJwtAuthentication(builder.Configuration);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.UseCors("AllowAll");
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
